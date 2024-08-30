2bcbb244ea0b9d88d49766da3e77cf752939f411
Medido1 <medi1222@gmail.com>
2024-08-30 07:48:45 +0100

    Set up desktop design for footer and do some tweaks in main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,24 +6,30 @@ export default function Footer() {
   return (
     <footer className="footer">
       <img src={logo} alt='logo' className='logo'/>
-      <p className='footer_heading'>Product</p>
-      <ul className='footer_links'>
-        {products.map(item =>
-          <li key={item.id}>{item.name}</li>
-        )}
-      </ul>
-      <p className="footer_heading">Company</p>
-      <ul className='footer_links'>
-        {company.map(item =>
-          <li key={item.id}>{item.name}</li>
-        )}
-      </ul>
-      <p className="footer_heading">Connect</p>
-      <ul className='footer_links'>
-        {connect.map(item =>
-          <li key={item.id}>{item.name}</li>
-        )}
-      </ul>
+      <div>
+        <p className='footer_heading'>Product</p>
+        <ul className='footer_links'>
+          {products.map(item =>
+            <li key={item.id}>{item.name}</li>
+          )}
+        </ul>
+      </div>
+      <div>
+        <p className="footer_heading">Company</p>
+        <ul className='footer_links'>
+          {company.map(item =>
+            <li key={item.id}>{item.name}</li>
+          )}
+        </ul>
+      </div>
+      <div>
+        <p className="footer_heading">Connect</p>
+        <ul className='footer_links'>
+          {connect.map(item =>
+            <li key={item.id}>{item.name}</li>
+          )}
+        </ul>
+      </div>
     </footer>
   )
 }
